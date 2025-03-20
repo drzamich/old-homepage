10c2244e146497072c74e439f672e115daf7834e
Michał Drzazga <drzamich@gmail.com>
2025-03-20 10:10:27 +0100

    finish docs

--- docs/script/blad.js
+++ docs/script/blad.js
@@ -21,7 +21,7 @@ document.write(
   new Array(
     // Tu wpisz kolejne elementy:
 
-    '<h4>No document type declaration; implying "&lt;!DOCTYPE HTML SYSTEM&gt;"</h4><p>Taki błąd oznacza, że na początku dokumentu nie zadeklarowano w jakim typie języka HTML napisana jest strona WWW, czyli <span class="i">Doctype</span>. Aby go naprawić należy to zrobić. Jeśli nie wiesz jaki <span class="i">Doctype</span> zadeklarować, przeczytaj <a href="porady-1.php">porady XHTML</a>.</p>',
+    '<h4>No document type declaration; implying "&lt;!DOCTYPE HTML SYSTEM&gt;"</h4><p>Taki błąd oznacza, że na początku dokumentu nie zadeklarowano w jakim typie języka HTML napisana jest strona WWW, czyli <span class="i">Doctype</span>. Aby go naprawić należy to zrobić. Jeśli nie wiesz jaki <span class="i">Doctype</span> zadeklarować, przeczytaj <a href="porady-1.html">porady XHTML</a>.</p>',
 
     '<h4>End tag for element "<var>znacznik</var>" which is not open.</h4><p>Taki błąd oznacza, że został zamknięty <var>znacznik</var>, który nie został otwarty nigdzie wcześniej. Należy go wykasować.</p>',
 
@@ -37,7 +37,7 @@ document.write(
 
     '<h4>Required attribute "<var>atrybut</var>" not specified</h4><p>Wymagany w danym znaczniku atrybut nie został określony. Może dziać się tak na przykład gdy znacznikowi <code>&lt;style&gt;</code> nie nadamy atrybutu <code>type</code></p>',
 
-    '<h4>Missing xmlns attribute for element html. The value should be (...)</h4><p>Przy określaniu <acronym title="Extensible HyperText Markup Language">XHTML</acronym> znacznikowi <code>&lt;html&gt;</code> nie nadano atrybutu <code>xmlns</code>. Przykład użycia tego atrybutu można znaleźć w dziale <a href="fundament.php">Fundament kodu</a>.</p>',
+    '<h4>Missing xmlns attribute for element html. The value should be (...)</h4><p>Przy określaniu <acronym title="Extensible HyperText Markup Language">XHTML</acronym> znacznikowi <code>&lt;html&gt;</code> nie nadano atrybutu <code>xmlns</code>. Przykład użycia tego atrybutu można znaleźć w dziale <a href="fundament.html">Fundament kodu</a>.</p>',
 
     '<h4>End tag for "<var>znacznik</var>" which is not finished.</h4><p>W tym przypadku są dwie możliwe przyczyny tego błędu:<ul><li>Jeśli w w jakimś znaczniku (na przykład <code>&lt;p&gt;</code>) umieścimy inny znacznik (na przyk-ad <code>&lt;em&gt;</code>), to musimy zamkn-c go przed zamknięciem tego pierwszego. Na przykład: &lt;p&gt;...&lt;em&gt;...&lt;/em&gt;...&lt;/p&gt;</li><li>Niektóre znaczniki <span class="u">muszą</span> zawierac w sobie jakieś inne. Na przykład znacznik <code>&lt;head&gt;</code> musi zawierać w sobie znacznik <code>&lt;title&gt;</code>, znacznik  <code>&lt;&gt;</code> znacznik <code>&lt;li&gt;</code> i tak dalej...</li></ul></p>',
 
